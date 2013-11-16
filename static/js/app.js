6e6684432703ae00a7ea3a6b7a45d9dc7c4478c5
aicioara <primulandrei@yahoo.com>
2013-11-16 17:09:04 +0000

    Changed indentation

--- static/js/app.js
+++ static/js/app.js
@@ -1,30 +1,30 @@
 var dashboard = angular.module('dashboard', []);
 
 dashboard.service('IssuesService', function() {
-    this.issues = [{text:"Issue here", time: new Date()}, {text:"Issues 2 eye", time: new Date()}];
+	this.issues = [{text:"Issue here", time: new Date()}, {text:"Issues 2 eye", time: new Date()}];
 });
 
 dashboard.controller('IssuesController', function($scope, IssuesService){
-    $scope.newIssue = {};
-    $scope.issuesService = IssuesService;
+	$scope.newIssue = {};
+	$scope.issuesService = IssuesService;
 
-    $scope.addIssue = function(newIssue) {
-      newIssue.time = new Date();
-      IssuesService.issues.push(newIssue);
-      $scope.newIssue = newIssue = {};
-    }
+	$scope.addIssue = function(newIssue) {
+	  newIssue.time = new Date();
+	  IssuesService.issues.push(newIssue);
+	  $scope.newIssue = newIssue = {};
+	}
 })
 
 dashboard.directive('ngEnter', function () {
-    return function (scope, element, attrs) {
-        element.bind("keydown keypress", function (event) {
-            if(event.which === 13) {
-                scope.$apply(function (){
-                    scope.$eval(attrs.ngEnter);
-                });
+	return function (scope, element, attrs) {
+		element.bind("keydown keypress", function (event) {
+			if(event.which === 13) {
+				scope.$apply(function (){
+					scope.$eval(attrs.ngEnter);
+				});
 
-                event.preventDefault();
-            }
-        });
-    };
+				event.preventDefault();
+			}
+		});
+	};
 });
