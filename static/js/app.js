a19c927bc69b96179737c4107e07d2d6b145ee46
Nicola Greco <notsecurity@gmail.com>
2013-11-16 19:10:00 +0000

    implementing new graph

--- static/js/app.js
+++ static/js/app.js
@@ -33,6 +33,94 @@ dashboard.filter('fromNow', function () {
   };
 });
 
+dashboard.directive('d3Issues', function($window) {
+  return {
+    restrict: 'EA',
+    scope: {},
+    link: function(scope, element, attrs) {
+
+      // Browser onresize event
+      $window.onresize = function() {
+        scope.$apply();
+      };
+
+      // hard-code data
+      scope.data = [
+        {name: "Greg", score: 98},
+        {name: "Ari", score: 96},
+        {name: 'Q', score: 75},
+        {name: "Loser", score: 48}
+      ];
+
+      // Watch for resize event
+      scope.$watch(function() {
+        return angular.element($window)[0].innerWidth;
+      }, function() {
+        scope.render(scope.data);
+      });
+
+      var margin = {top: 20, right: 20, bottom: 30, left: 50},
+        width = 960 - margin.left - margin.right,
+        height = 500 - margin.top - margin.bottom;
+
+      var parseDate = d3.time.format("%d-%b-%y").parse;
+
+      var x = d3.time.scale()
+        .range([0, width]);
+
+      var y = d3.scale.linear()
+        .range([height, 0]);
+
+      var xAxis = d3.svg.axis()
+        .scale(x)
+        .orient("bottom");
+
+      var yAxis = d3.svg.axis()
+        .scale(y)
+        .orient("left");
+
+      var area = d3.svg.area()
+        .x(function(d) { return x(d.date); })
+        .y0(height)
+        .y1(function(d) { return y(d.close); });
+
+      scope.render = function(data) {
+        // remove all previous items before render
+
+        var svg = d3.select(element[0]).append("svg")
+          .attr("width", width + margin.left + margin.right)
+          .attr("height", height + margin.top + margin.bottom)
+          .append("g")
+          .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+          x.domain(d3.extent(data, function(d) { return d.date; }));
+          y.domain([0, d3.max(data, function(d) { return d.close; })]);
+
+          svg.append("path")
+            .datum(data)
+            .attr("class", "area")
+            .attr("d", area);
+
+          svg.append("g")
+            .attr("class", "x axis")
+            .attr("transform", "translate(0," + height + ")")
+            .call(xAxis);
+
+          svg.append("g")
+            .attr("class", "y axis")
+            .call(yAxis)
+            .append("text")
+            .attr("transform", "rotate(-90)")
+            .attr("y", 6)
+            .attr("dy", ".71em")
+            .style("text-anchor", "end")
+            .text("Price ($)");
+      }
+    }
+  }
+});
+
+
 dashboard.directive('ngEnter', function () {
 	return function (scope, element, attrs) {
 		element.bind("keydown keypress", function (event) {
