1fb11d73411f8ee98f64b8e5c7f01d7e0ad307a0
Nicola Greco <notsecurity@gmail.com>
2013-11-17 00:26:53 +0000

    graph showing checks

--- static/js/app.js
+++ static/js/app.js
@@ -171,7 +171,6 @@ dashboard.directive('d3Issues', function($window, IssuesService) {
 
       scope.render = function(data) {
 
-        var data = calculate_frequency(data);
 
         var margin = {top: 20, right: 20, bottom: 30, left: 50},
           width = 730 - margin.left - margin.right,
@@ -215,7 +214,31 @@ dashboard.directive('d3Issues', function($window, IssuesService) {
           .append("g")
           .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-        var data = data.map(function(d) {
+        var data = calculate_frequency(data);
+
+        var circlePoints = _.filter(data, function(issues){
+          console.log("issues", issues[1]);
+          var allTheSolved = _.all(issues[1], function(issue) { return issues[0] != moment(issue.time).format("YYYY-MM-DD")});
+          return !allTheSolved;
+        }).map(function(d) {
+          return {
+            date: parseDate(d[0]),
+            close: d[1].length
+          };
+        });
+
+        var ticksPoints = _.filter(data, function(issues){
+          console.log("issues", issues[1]);
+          var allTheSolved = _.all(issues[1], function(issue) { return issues[0] != moment(issue.time).format("YYYY-MM-DD")});
+          return allTheSolved;
+        }).map(function(d) {
+            return {
+              date: parseDate(d[0]),
+              close: d[1].length
+            };
+          });
+
+        var linePoints = data.map(function(d) {
           return {
             date: parseDate(d[0]),
             close: d[1].length
@@ -223,11 +246,11 @@ dashboard.directive('d3Issues', function($window, IssuesService) {
 
         });
 
-        console.log(data);
+        console.log(linePoints);
 
 
-        x.domain(d3.extent(data, function(d) { return d.date; }));
-        y.domain(d3.extent(data, function(d) { return d.close; }));
+        x.domain(d3.extent(linePoints, function(d) { return d.date; }));
+        y.domain(d3.extent(linePoints, function(d) { return d.close; }));
 
         svg.append("clipPath")
           .attr("id", "clip")
@@ -239,7 +262,7 @@ dashboard.directive('d3Issues', function($window, IssuesService) {
         svg.append("path")
           .attr("class", "area")
           .attr("clip-path", "url(#clip)")
-          .attr("d", area(data));
+          .attr("d", area(linePoints));
 
         // Add the x-axis.
         svg.append("g")
@@ -255,18 +278,26 @@ dashboard.directive('d3Issues', function($window, IssuesService) {
 
         // Add the line path.
         svg.append("path")
-          .datum(data)
+          .datum(linePoints)
           .attr("class", "line")
           .attr("d", line);
 
         var points = svg.selectAll(".point")
-          .data(data)
+          .data(circlePoints)
           .enter().append("svg:circle")
           .attr("class", 'point')
-          .attr("fill", function(d, i) { return "black" })
           .attr("cx", function(d, i) { return x(d.date) })
           .attr("cy", function(d, i) { return y(d.close) })
           .attr("r", function(d, i) { return 6 });
+
+        var tickPoint = svg.selectAll(".tickPoint")
+          .data(ticksPoints)
+          .enter().append("svg:text")
+          .attr("class", 'tickPoint')
+          .attr("x", function(d, i) { return x(d.date) + 10 })
+          .attr("y", function(d, i) { return y(d.close) - 10 })
+          .attr("r", function(d, i) { return 6 })
+          .text(function(d) {return "\u2714"})
 
       }
     }
